2ff8fc3267bd8532000363c611a75b55afad9c02
mohammed el aouri <mohammedelaouri@gmail.com>
2025-03-17 22:50:49 +0000

    `Implementing the Game Over functionality`

--- 4- Hangman-Game/javaScript/script.js
+++ 4- Hangman-Game/javaScript/script.js
@@ -1,17 +1,38 @@
 const keyboard=document.querySelector(".keyboard");
 const hintText=document.querySelector(".hint-text b");
-const wordDisplay=document.querySelector(".word-display")
+const wordDisplay=document.querySelector(".word-display");
+const guessesText=document.querySelector(".guesses-text b");
+const hangmanImage=document.querySelector("[alt='hangman-img']")
+let currentWord, counter=0
+const MAX_GUESSES=6;
 // get random Words and hint from random words List
 const getRandomWords=()=>{
-    let {word,hint}=wordList[Math.floor(Math.random()*wordList.length)];
+let {word,hint}=wordList[Math.floor(Math.random()*wordList.length)];
     hintText.innerHTML=`Hint:<b>${hint}</b>`;
-    wordDisplay.innerHTML=word.split('').map(_=>`<li class="letter"></li>`).join('')
+    wordDisplay.innerHTML=word.split('').map(_=>`<li class="letter"></li>`).join('');
+    currentWord=word;
 }
 getRandomWords();
 //
-const inGame=()=>{
+const inGame=(button,clickedWord)=>{
+        // Checking if clickedLetter is Existe on the Current word
+        if(currentWord.includes(clickedWord)){
+            [...currentWord].forEach((char,index)=>{
+                if(char===clickedWord){
+                    console.log(char)
+                    wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+                    wordDisplay.querySelectorAll("li")[index].innerText=char;
+                }
+            })
+        }else {
+            counter++;
+            hangmanImage.src=`assets/images/hangman-${counter}.svg`;
+        }
+        button.disabled=true;
+        guessesText.innerHTML=`${counter}/${MAX_GUESSES}`
+};
 
-}
+
 // create KeyBord buttons dynamically and Add key Bord Listener
 for(let i=97 ;i<=122 ;i++){
     const button=document.createElement("button");
