4a058c06f5dde921aa25bc864a834ddb90c70e5a
mohammed el aouri <mohammedelaouri@gmail.com>
2025-03-17 01:31:04 +0000

    `Displaying a random word and hint`

--- 4- Hangman-Game/javaScript/script.js
+++ 4- Hangman-Game/javaScript/script.js
@@ -1,9 +1,21 @@
 const keyboard=document.querySelector(".keyboard");
+const hintText=document.querySelector(".hint-text b");
+const wordDisplay=document.querySelector(".word-display")
+// get random Words and hint from random words List
+const getRandomWords=()=>{
+    let {word,hint}=wordList[Math.floor(Math.random()*wordList.length)];
+    hintText.innerHTML=`Hint:<b>${hint}</b>`;
+    wordDisplay.innerHTML=word.split('').map(_=>`<li class="letter"></li>`).join('')
+}
+getRandomWords();
+//
+const inGame=()=>{
 
-// create KeyBord buttons dynamically
+}
+// create KeyBord buttons dynamically and Add key Bord Listener
 for(let i=97 ;i<=122 ;i++){
     const button=document.createElement("button");
     button.innerText=String.fromCharCode(i);
     keyboard.appendChild(button)
+    button.addEventListener("click",e=>inGame(e.target,String.fromCharCode(i)))
 }
-// 
