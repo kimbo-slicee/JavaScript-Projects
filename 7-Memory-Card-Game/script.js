6a1c6d8474b2b1c0225d06bd89e39c3b12d798b9
mohammed el aouri <mohammedelaouri@gmail.com>
2025-03-26 00:03:57 +0000

    `handleCardClick Event And start the Game`

--- 7-Memory-Card-Game/script.js
+++ 7-Memory-Card-Game/script.js
@@ -3,30 +3,37 @@ let selectedCards=[];
 let canClick = true
 
 // handel card click
-const handelCardClick=(e)=>{
+const handleCardClick = (e) => {
     if (!canClick || selectedCards.includes(e.currentTarget) || e.currentTarget.classList.contains("matched")) {
         return;
     }
     e.currentTarget.classList.remove("flip");
-    let cardIndex=e.currentTarget.querySelector("img").src.match(/\d+/gi)[2];
-    console.log(cardIndex)
-    selectedCards.push(cardIndex)
-    if(selectedCards.length===2){
-        if(selectedCards[0]===selectedCards[1]){
-            e.currentTarget.classList.add("flip");
-            selectedCards=[];
-        }else {
-            e.currentTarget.classList.remove("flip");
-            selectedCards=[];
+    e.currentTarget.classList.add("clicked")
+    // Extract the card's unique index from its image source
+    let cardIndex = e.currentTarget.querySelector("img").src.match(/\d+/gi)[2];
+    if(canClick) selectedCards.push({ element: e.currentTarget, index: cardIndex });
+    if (selectedCards.length === 2) {
+        if (selectedCards[0].index === selectedCards[1].index) {
+            selectedCards = [];
+        } else {
+            console.log("false");
+            // Store references to the selected elements
+            let [firstCard, secondCard] = [selectedCards[0].element, selectedCards[1].element];
+            setTimeout(() => {
+                firstCard.classList.add("flip");
+                secondCard.classList.add("flip");
+            }, 500);
+            selectedCards = [];
         }
     }
-}
+};
+// shoe card for 2s and flip cards
 const startGame=()=>{
     setTimeout(()=>{
         // hide All card after 2s before staring the game
         cards.forEach(card=>{
             card.classList.add("flip");
-            card.addEventListener("click",handelCardClick)
+            card.addEventListener("click",handleCardClick)
         },2000);
     })
 }
