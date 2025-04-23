c87a53b1679ff05e2b0542134a2e502d464200a9
mohammed el aouri <mohammedelaouri@gmail.com>
2025-04-23 17:16:26 +0100

    `Working on Generating Random Password`

--- 10-Random-Password-Generator/script.js
+++ 10-Random-Password-Generator/script.js
@@ -3,6 +3,8 @@ const passwordLengthValue=document.querySelector(".pass-length .details span");
 const generatePasswordBtn=document.querySelector(".generate-btn");
 const options=document.querySelectorAll(".options .option input");
 const input =document.querySelector("input[type='text']");
+let excludeDuplicate=false
+
 
 const characters={
    Lowercase:"abcdefghijklmnopqrstuvwxyz",
@@ -15,24 +17,46 @@ const updateSlide=()=>{
    passwordLengthValue.innerText=lengthSlider.value;
    generatePassword();
 }
+const randomPass=(password)=>{
+   let randomPassword="",
+       randomChar="",
+       passLength=lengthSlider.value;
+   for(let i=0; i<passLength; i++){
+       randomChar=password[Math.floor(Math.random()*password.length)];
+       if(excludeDuplicate){
+          // randomPassword=[...new Set(randomPassword.split(''))].join('');
+          if(!randomPassword.includes(randomChar) || randomChar===" "){
+             randomPassword+=randomChar;
+          }else{
+             i--;
+          }
+       }else {
+          randomPassword+=randomChar;
+       }
+   }
+   return randomPassword;
+}
 
 const generatePassword=()=>{
    let staticPassword="";
-   let randomPassword="";
-   let passLength=lengthSlider.value;
    options.forEach((option)=>{
       if(option.checked){
+         if(option.id!=="exc-duplicate" && option.id!=="spaces"){
          // use the input id to know which one of them is checked then add it to staticPassword variable it's like
          // kind of filtrate
          staticPassword+=characters[option.id];
+         }else if(option.id==="spaces"){
+            //add space if the use check space checkBox
+            staticPassword+=` ${staticPassword} `
+         }else {
+            //remove duplicate characters
+            excludeDuplicate=true;
+         }
       }
    })
-   for(let i=0;i<passLength;i++){
-      randomPassword+=staticPassword[Math.floor(Math.random()*staticPassword.length)];
-   }
-   input.value=randomPassword;
+   // random Password
+   input.value=randomPass(staticPassword);
 }
-
 updateSlide();
 lengthSlider.addEventListener("input",updateSlide);
 generatePasswordBtn.addEventListener("click",generatePassword)
